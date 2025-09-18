a1fc16c1b9a7cf92c5a578129a06493b5890e47b
MohamedCoderX <mdibrahim.developer@gmail.com>
2025-09-18 10:55:05 +0530

    asdas

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+<h1>LOGO</h1>
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div>
+           <button>Contact Us</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
